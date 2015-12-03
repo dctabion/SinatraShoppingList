ddfa8804045fb6f895068c6ae4252965c80f899f
Daniel Tabion <dctabion@gmail.com>
2015-12-02 18:23:21 -0600

    made registration and list routes & views

--- app.rb
+++ app.rb
@@ -12,3 +12,35 @@ end
 get '/registration' do
   erb :registration
 end
+
+post '/registration_validation' do
+  puts params
+  reg_status = 'Developer made a mistake!'
+
+  # ??? TODO add check if valid email address
+  # ??? TODO add check if passwords are not null or too long
+
+  # check if passwords
+  # valid: passwords & password confirmation match
+  if params[:password] != ''
+    p 'password is not empty'
+    if params[:password] == params[:password_conf]
+      p 'passwords match'
+      @email = params[:email]
+      erb :registration_success
+    # invalid: password & password confirmation DO NOT match
+    elsif
+      @reg_status = 'Password & confirmation DO NOT match!'
+      erb :registration_fail
+    end
+  elsif
+    @reg_status = 'Invalid password!'
+    erb :registration_fail
+  end
+
+end
+
+
+get '/list' do
+  erb :list
+end
